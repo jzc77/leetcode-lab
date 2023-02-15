99f2322f6f889c7c18e4125ca6a10f4f73de2d0a
JazC <jchan7713@gmail.com>
2023-02-14 23:12:55 -0800

    Passing 65 out of 71 test cases on Leetcode.com because time limit exceeded

--- src/ContainsDuplicate217.java
+++ src/ContainsDuplicate217.java
@@ -15,13 +15,22 @@ import java.util.Arrays;
 
 public class ContainsDuplicate217 {
     public boolean containsDuplicate217(int[] nums) {
-        System.out.println(Arrays.toString(nums));
-
+        Integer[] newIntArray = new Integer[nums.length];
+        System.out.println(nums.length);
+        for (int i=0; i<nums.length; i++) {
+            if(Arrays.asList(newIntArray).contains(nums[i])) {
+                return true;
+            } else {
+                newIntArray[i] = nums[i];
+            }
+        }
         return false;
     }
 
     public static void main(String[] args) {
-        int[] intArray = new int[] {4,5,6,7,8};
+        //int[] intArray = new int[] {1,2,3,1};  // true
+        //int[] intArray = new int[] {1,2,3,4};  // false
+        int[] intArray = new int[] {1,1,1,3,3,4,3,2,4,2};  // true
         ContainsDuplicate217 containsDuplicate217 = new ContainsDuplicate217();
         System.out.println(containsDuplicate217.containsDuplicate217(intArray));
     }
