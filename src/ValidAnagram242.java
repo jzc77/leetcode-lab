54e351fea013f4579a372d869d7821312de0b811
JazC <jchan7713@gmail.com>
2023-02-17 16:13:41 -0800

    Add approach 2 for ValidAnagram242; passing 25/38 leetcode.com test cases

--- src/ValidAnagram242.java
+++ src/ValidAnagram242.java
@@ -2,23 +2,31 @@
 
 /*
 Thought process:
+(Approach 1)
 -Sort both input strings
 -Then compare if they are equal
     - If they are equal, return true (all the letters in one string are in the other)
     - Else they are not equal, return false
+
+(Approach 2)
+-First, turn both strings into char arrays
+-For each char in string 1, check to see if it's also in string 2.
+    -If not, return false (not a valid anagram)
+    -If so, remove the char from string 2
+-At the end of string 1, if string 2 is empty, return true (they were anagrams)
  */
 
 import java.util.Arrays;
 
 public class ValidAnagram242 {
 
-    public static String sortString(String inputString) {
+    public static String sortString(String inputString) {  // Helper function for isAnagram()
         char tempArray[] = inputString.toCharArray();
         Arrays.sort(tempArray);
         return new String(tempArray);
     }
 
-    public boolean isAnagram(String s, String t) {
+    public boolean isAnagram(String s, String t) {  // Uses helper function above
         String sorted_s = sortString(s);
         String sorted_t = sortString(t);
 
@@ -29,6 +37,40 @@ public class ValidAnagram242 {
         }
     }
 
+    public boolean containsChar(char character, char[] charArray) {  // Helper function for isAnagram_approach2()
+        for (char ch : charArray) {
+            if (ch == character) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public char[] removeChar(char character, char[] charArray) {  // Helper function for isAnagram_approach2()
+        char[] newCharArray = new char[(charArray.length)];
+        for (int i=0; i < charArray.length; i++) {
+            if (character == charArray[i]) {
+                newCharArray[i] = charArray[i];
+            }
+        }
+        return newCharArray;
+    }
+
+    public boolean isAnagram_approach2(String s, String t) {  // Uses two helper functions above
+        char[] charArray_s = s.toCharArray();
+        for (int i=0; i < charArray_s.length; i++) {
+            char[] charArray_t = t.toCharArray();
+            if (containsChar(charArray_s[i], charArray_t) == false) {
+                System.out.println("return false 2");
+                return false;
+            } else {
+                charArray_t = removeChar(charArray_s[i], charArray_t);
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         String s1 = "anagram";
         String t1 = "nagaram";
@@ -36,8 +78,13 @@ public class ValidAnagram242 {
         String s2 = "rat";
         String t2 = "car";
 
+        // For approach 1
+        //ValidAnagram242 validAnagram242 = new ValidAnagram242();
+        //System.out.println(validAnagram242.isAnagram(s1, t1));
+        //System.out.println(validAnagram242.isAnagram(s2, t2));
+
+        // For approach 2
         ValidAnagram242 validAnagram242 = new ValidAnagram242();
-        System.out.println(validAnagram242.isAnagram(s1, t1));
-        System.out.println(validAnagram242.isAnagram(s2, t2));
+        System.out.println(validAnagram242.isAnagram_approach2(s1, t1));
     }
 }
