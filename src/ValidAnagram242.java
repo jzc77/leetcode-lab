909ad613bbe2908502572f8a85b851d0f08a96f1
JazC <jchan7713@gmail.com>
2023-02-18 12:29:38 -0800

    Edit approach 2 for ValidAnagram242; time-limit exceeded, but passing 35/38 leetcode.com test cases

--- src/ValidAnagram242.java
+++ src/ValidAnagram242.java
@@ -47,27 +47,42 @@ public class ValidAnagram242 {
     }
 
     public char[] removeChar(char character, char[] charArray) {  // Helper function for isAnagram_approach2()
-        char[] newCharArray = new char[(charArray.length)];
+        char[] newCharArray = new char[(charArray.length - 1)];
+        int newIndex = 0;
+        boolean skippedStatus = false;
         for (int i=0; i < charArray.length; i++) {
-            if (character == charArray[i]) {
-                newCharArray[i] = charArray[i];
+            if (character != charArray[i]) {  // if characters don't match, put it into new array and increase newIndex
+                newCharArray[newIndex] = charArray[i];
+                newIndex++;
+            } else if (character == charArray[i] && skippedStatus) { // the same character is encountered in the charArray again
+                newCharArray[newIndex] = charArray[i];
+                newIndex++;
+            } else {  // skip it for only one time and continue
+                skippedStatus = true;
             }
         }
         return newCharArray;
     }
 
-    public boolean isAnagram_approach2(String s, String t) {  // Uses two helper functions above
+    public boolean isAnagram_approach2(String s, String t) {  // Uses two helper functions above; passing 25/38 leetcode.com test cases
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
         char[] charArray_s = s.toCharArray();
+        char[] charArray_t = t.toCharArray();
         for (int i=0; i < charArray_s.length; i++) {
-            char[] charArray_t = t.toCharArray();
             if (containsChar(charArray_s[i], charArray_t) == false) {
-                System.out.println("return false 2");
                 return false;
             } else {
                 charArray_t = removeChar(charArray_s[i], charArray_t);
+                // If at the end of string s and string t is not 0, return false
+                if ((i == charArray_s.length - 1) && charArray_t.length != 0) {
+                    return  false;
+                }
             }
         }
-
         return true;
     }
 
@@ -78,6 +93,12 @@ public class ValidAnagram242 {
         String s2 = "rat";
         String t2 = "car";
 
+        String s3 = "a";
+        String t3 = "ab";
+
+        String s4 = "aacc";
+        String t4 = "ccac";
+
         // For approach 1
         //ValidAnagram242 validAnagram242 = new ValidAnagram242();
         //System.out.println(validAnagram242.isAnagram(s1, t1));
@@ -85,6 +106,6 @@ public class ValidAnagram242 {
 
         // For approach 2
         ValidAnagram242 validAnagram242 = new ValidAnagram242();
-        System.out.println(validAnagram242.isAnagram_approach2(s1, t1));
+        System.out.println(validAnagram242.isAnagram_approach2(s4, t4));
     }
 }
