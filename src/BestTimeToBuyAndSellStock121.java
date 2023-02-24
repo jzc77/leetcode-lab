060a2e38d50c4ced35da01b5f3b76ea7feedcde3
JazC <jchan7713@gmail.com>
2023-02-23 22:00:40 -0800

    Add thought process to BestTimeToBuyAndSellStock

---
+++ src/BestTimeToBuyAndSellStock121.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+/*
+Thought process:
+-Need two pointers, one for buy (left) and one for sell (right)
+(We want the left pointer to have the lowest value and the right pointer to have the highest value for maximum profit)
+-Also, need to initialize a maxProfit variable to keep track of the highest profit. Initialize to 0.
+-Loop through the prices int array, with left and right pointers starting at index 0 and 1, respectively
+    -If the right pointer has a lower value than the current left pointer, move the left pointer one index to the right
+    -If the right pointer encounters a higher value (as initialized as the right pointer), update the right pointer to
+     that higher value
+    -Update the maxProfit variable to the difference between left and right pointers, if it is greater than the current
+-Return maxProfit
+ */
+
+public class BestTimeToBuyAndSellStock121 {
+
+    public static int maxProfit(int[] prices) {
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
