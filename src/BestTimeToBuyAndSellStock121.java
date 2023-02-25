c7646dee6eaec1a03abe11882fb4b4e56cc04cd7
JazC <jchan7713@gmail.com>
2023-02-25 15:23:02 -0800

    Passing all leetcode test cases, looked up solution

--- src/BestTimeToBuyAndSellStock121.java
+++ src/BestTimeToBuyAndSellStock121.java
@@ -13,35 +13,50 @@ Thought process:
 -Return maxProfit
  */
 
-import java.util.Arrays;
-import java.util.HashSet;
-
 public class BestTimeToBuyAndSellStock121 {
 
+//    public static int maxProfit(int[] prices) {
+//        int left = 0;
+//        int right = 1;
+//        int maxProfit = 0;
+//
+//        if (prices.length == 1) {
+//            return maxProfit;
+//        }
+//
+//        if (prices[right] == prices[left] && prices.length == 2) {
+//            return maxProfit;
+//        }
+//
+//
+//        for (int i=0; i<(prices.length); i++) {
+//            if (prices[right] - prices[left] > maxProfit) {
+//                maxProfit = prices[right] - prices[left];
+//            } else {
+//                if (prices[right] < prices[left]) {
+//                    left = right;
+//                } else {
+//                    right++;
+//                }
+//            }
+//        }
+//        return maxProfit;
+//    }
+
+    // From neetcode video solution
     public static int maxProfit(int[] prices) {
         int left = 0;
         int right = 1;
         int maxProfit = 0;
 
-        if (prices.length == 1) {
-            return maxProfit;
-        }
-
-        if (prices[right] == prices[left] && prices.length == 2) {
-            return maxProfit;
-        }
-
-
-        for (int i=0; i<(prices.length); i++) {
-            if (prices[right] - prices[left] > maxProfit) {
-                maxProfit = prices[right] - prices[left];
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                int profit = prices[right] - prices[left];
+                maxProfit = Math.max(maxProfit, profit);
             } else {
-                if (prices[right] < prices[left]) {
-                    left = right;
-                } else {
-                    right++;
-                }
+                left = right;
             }
+            right++;
         }
         return maxProfit;
     }
@@ -50,8 +65,8 @@ public class BestTimeToBuyAndSellStock121 {
         //int[] input = {7,1,5,3,6,4}; // 5
         //int[] input = {7,6,4,3,1}; // 0
         //int[] input = {1,2}; // 1
-        //int[] input = {2,1,4}; // 3
-        int[] input = {3,3}; // 0
+        int[] input = {2,1,4}; // 3
+        //int[] input = {3,3}; // 0
         System.out.println(maxProfit(input));
     }
 }
