7a4b3063851f8b75431f26b86e45b9c8cf474f04
JazC <jchan7713@gmail.com>
2023-03-10 20:36:01 -0800

    Solved stringBits warmup2

--- src/CodingBat/Warmup2/stringBits.java
+++ src/CodingBat/Warmup2/stringBits.java
@@ -1,14 +1,24 @@
 package CodingBat.Warmup2;
-public class stringBits {
+// https://codingbat.com/prob/p165666
+/*
+Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
 
-    public static String helloWorld() {
-        System.out.println("this is test");
-        return "Hello world";
+stringBits("Hello") → "Hlo"
+stringBits("Hi") → "H"
+stringBits("Heeololeo") → "Hello"
+ */
+public class stringBits {
+    public static String stringBits(String str) {
+        String result = "";
+        for (int i=0; i<str.length(); i++) {
+            if (i % 2 == 0) {
+                result += str.charAt(i);
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        stringBits template = new stringBits();
-        template.helloWorld();
-
+        System.out.println(stringBits("Hello"));
     }
 }
