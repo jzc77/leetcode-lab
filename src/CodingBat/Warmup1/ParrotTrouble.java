afbd1245d45f0717219a01f65752233ed106ab10
JazC <jchan7713@gmail.com>
2023-03-27 21:46:24 -0700

    Pass all test cases for ParrotTrouble

---
+++ src/CodingBat/Warmup1/ParrotTrouble.java
@@ -0,0 +1,25 @@
+package CodingBat.Warmup1;
+// https://codingbat.com/prob/p140449
+/*
+We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23.
+We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+
+parrotTrouble(true, 6) → true
+parrotTrouble(true, 7) → false
+parrotTrouble(false, 6) → false
+ */
+public class ParrotTrouble {
+    public static boolean parrotTrouble(boolean talking, int hour) {
+        if (talking && (hour<7 || hour>20)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        boolean talking = false;
+        int hour = 6;
+        System.out.println(parrotTrouble(talking, hour));
+    }
+}
