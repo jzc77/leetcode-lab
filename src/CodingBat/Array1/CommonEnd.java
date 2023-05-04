b9c7304497b2ddb52115abb59b77cbacc6cde3a5
JazC <jchan7713@gmail.com>
2023-05-04 09:23:00 -0700

    Solve DateFashion

--- src/CodingBat/Array1/CommonEnd.java
+++ src/CodingBat/Array1/CommonEnd.java
@@ -1,7 +1,7 @@
 package CodingBat.Array1;
 // https://codingbat.com/prob/p191991
 /*
-Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element.
+Given 2 arrays of ints, a and b, return true if they have the same first element, or they have the same last element.
 Both arrays will be length 1 or more.
 
 commonEnd([1, 2, 3], [7, 3]) → true
